4a1d06406db5e8bd8cac53bd2e1be15df3cf971d
davideerusso <rd.96.rd@gmail.com>
2025-05-29 16:09:34 +0200

    set check

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,81 @@ const initialData = { author: "", title: "", body: "", public: false };
 
 export default function App() {
   const [formData, setFormData] = useState(initialData);
-  const [isChecked, setIsChecked] = useState(false);
 
   const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-    setIsChecked(!isChecked);
+    /**
+     * se e.target è di tipo "checkbox"
+     * allora value = e.target.checked
+     * altrimenti e.target.value
+     */
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+    setFormData({ ...formData, [e.target.name]: value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
   };
   console.log(formData);
 
   return (
     <>
-      <form>
-        <input
-          className="input "
-          name="author"
-          value={formData.author}
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Author"
-        ></input>
-        <input
-          className="input "
-          name="title"
-          value={formData.title}
-          onChange={handleInputChange}
-          type="text"
-          placeholder="title"
-        ></input>
-        <textarea
-          className="input "
-          name="body"
-          value={formData.body}
-          onChange={handleInputChange}
-          type="text"
-          placeholder="body"
-        ></textarea>
-        <input
-          type="checkbox"
-          name="public"
-          value={formData.public}
-          checked={isChecked}
-          onChange={handleInputChange}
-        />
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <input
+            className="input "
+            name="author"
+            value={formData.author}
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Author"
+          ></input>
+        </div>
+        <div>
+          <input
+            className="input "
+            name="title"
+            value={formData.title}
+            onChange={handleInputChange}
+            type="text"
+            placeholder="title"
+          ></input>
+        </div>
+
+        <div>
+          <textarea
+            className="input "
+            name="body"
+            value={formData.body}
+            onChange={handleInputChange}
+            type="text"
+            placeholder="body"
+          ></textarea>
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            name="public"
+            checked={formData.public}
+            onChange={handleInputChange}
+          />
+          Rendi pubblica
+        </div>
         <button>Crea Post </button>
       </form>
+
+      {formData && (
+        <div>
+          <div>
+            <p>{formData.author}</p>
+            <h3>{formData.title}</h3>
+            <p>{formData.body}</p>
+            <p>{!formData.public ? "Pubblico" : "Bozza"}</p>
+          </div>
+        </div>
+      )}
     </>
   );
 }
