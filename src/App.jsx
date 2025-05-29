f5fb58270e64352fad2f4c37c72a5e50c461f355
davideerusso <rd.96.rd@gmail.com>
2025-05-29 15:39:41 +0200

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { useState } from "react";
-const initialData = { author: "", title: "", body: "", public: "" };
+const initialData = { author: "", title: "", body: "", public: false };
 
 export default function App() {
   const [formData, setFormData] = useState(initialData);
+  const [isChecked, setIsChecked] = useState(false);
+
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    setIsChecked(!isChecked);
   };
   console.log(formData);
 
@@ -15,10 +18,35 @@ export default function App() {
         <input
           className="input "
           name="author"
+          value={formData.author}
           onChange={handleInputChange}
           type="text"
-          placeholder="Inserisci testo"
+          placeholder="Author"
         ></input>
+        <input
+          className="input "
+          name="title"
+          value={formData.title}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="title"
+        ></input>
+        <textarea
+          className="input "
+          name="body"
+          value={formData.body}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="body"
+        ></textarea>
+        <input
+          type="checkbox"
+          name="public"
+          value={formData.public}
+          checked={isChecked}
+          onChange={handleInputChange}
+        />
+        <button>Crea Post </button>
       </form>
     </>
   );
